6577f68c39ec01c401a925739fa14c0eae80c919
Sylvain Arbour <blues1961@gmail.com>
2025-08-29 13:34:08 -0400

    add healthy check route

--- backend/calendar_api/urls.py
+++ backend/calendar_api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import CalendarViewSet, EventViewSet
+from .views_health import health
 
 router = DefaultRouter()
 router.register(r"calendars", CalendarViewSet, basename="calendar")
@@ -10,5 +11,6 @@ router.register(r"events", EventViewSet, basename="event")
 urlpatterns = [
     path("auth/jwt/create/", TokenObtainPairView.as_view(), name="jwt-create"),
     path("auth/jwt/refresh/", TokenRefreshView.as_view(), name="jwt-refresh"),
+    path("health/", health),
     path("", include(router.urls)),
 ]
