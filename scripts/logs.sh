5b38d8179b4e9542b17d4de432e6ed1bbcb34128
blues1961 <blues1961@gmail.com>
2026-05-11 10:06:26 -0400

    Corrige logs sans service

--- scripts/logs.sh
+++ scripts/logs.sh
@@ -7,8 +7,16 @@ set -a
 source .env
 set +a
 
-docker compose \
-  --env-file .env \
-  --env-file .env.local \
-  -f "docker-compose.${APP_ENV}.yml" \
-  logs -f "${1:-}"
+if [ $# -gt 0 ] && [ -n "${1:-}" ]; then
+  docker compose \
+    --env-file .env \
+    --env-file .env.local \
+    -f "docker-compose.${APP_ENV}.yml" \
+    logs -f "$1"
+else
+  docker compose \
+    --env-file .env \
+    --env-file .env.local \
+    -f "docker-compose.${APP_ENV}.yml" \
+    logs -f
+fi
